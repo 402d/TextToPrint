228fb652fa58e95ee64ca87a0cfcf0926658f00d
Muraveiko Oleg U <Muraveiko@users.noreply.github.com>
2020-08-27 15:31:43 +0300

    added clipboard

--- app/src/main/java/ru/a402d/texttoprint/MainActivity.java
+++ app/src/main/java/ru/a402d/texttoprint/MainActivity.java
@@ -2,6 +2,8 @@ package ru.a402d.texttoprint;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -35,10 +37,12 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Objects;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuCompat;
 
-public class MainActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler  {
+public class MainActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler {
     private double fontSize = 17;
     private double printFontSize = 26;
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 777;
@@ -58,19 +62,20 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
             "}" +
             "</style>" +
             "</head>" +
-            "<body>" ;
-    String htmlBody;
-    final String htmlFooter="</body></html>";
+            "<body>";
+    String htmlBody = "";
+    final String htmlFooter = "</body></html>";
 
     WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab =  findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
             bp.initialize();
         }
 
-        webView =  findViewById(R.id.wview);
+        webView = findViewById(R.id.wview);
 
         htmlBody = readAssets("html.html");
 
@@ -94,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
     }
 
     // read html body from assets
-    private String readAssets(@SuppressWarnings("SameParameterValue") String fileName){
+    private String readAssets(@SuppressWarnings("SameParameterValue") String fileName) {
         try {
             StringBuilder buf = new StringBuilder();
             InputStream inputStream = getAssets().open(fileName);
@@ -107,18 +112,18 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
             }
             in.close();
             return buf.toString();
-        }catch (Exception e){
+        } catch (Exception e) {
             return "";
         }
 
     }
 
-    private void parseIntent(Intent intent){
+    private void parseIntent(Intent intent) {
 
 
         String action = intent.getAction();
         if (action == null || action.equals(Intent.ACTION_MAIN)) {
-            webView.loadData(String.format(Locale.US,htmlHead,fontSize,printFontSize)+htmlBody+htmlFooter,"text/html; charset=utf-8","utf-8");
+            webView.loadData(String.format(Locale.US, htmlHead, fontSize, printFontSize) + htmlBody + htmlFooter, "text/html; charset=utf-8", "utf-8");
             return;
         }
         if (action.equals(Intent.ACTION_VIEW)) {
@@ -131,7 +136,8 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
         if (action.equals(Intent.ACTION_SEND)) {
             extras = intent.getExtras();
             if (extras == null) {
-                webView.loadData(String.format(Locale.US,htmlHead,fontSize,printFontSize)+htmlBody+htmlFooter,"text/html; charset=utf-8","utf-8");
+                htmlBody = "<h1>Error</h1><p>ACTION_SEND no Extras</p>";
+                webView.loadData(String.format(Locale.US, htmlHead, fontSize, printFontSize) + htmlBody + htmlFooter, "text/html; charset=utf-8", "utf-8");
                 return;
             }
             Uri uri = extras.getParcelable(Intent.EXTRA_STREAM);
@@ -141,7 +147,7 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
                 return;
             }
             if (stringText == null || stringText.trim().length() <= 0) {
-                webView.loadData(String.format(Locale.US,htmlHead,fontSize,printFontSize)+htmlBody+htmlFooter,"text/html; charset=utf-8","utf-8");
+                webView.loadData(String.format(Locale.US, htmlHead, fontSize, printFontSize) + htmlBody + htmlFooter, "text/html; charset=utf-8", "utf-8");
                 return;
             }
             stringBePrinted(stringText);
@@ -149,18 +155,18 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
 
     }
 
-    private void stringBePrinted(String s){
+    private void stringBePrinted(String s) {
         String[] separated = s.split("\n");
-        StringBuilder ss= new StringBuilder();
-        for (String v : separated ) {
+        StringBuilder ss = new StringBuilder();
+        for (String v : separated) {
             ss.append(v);
             ss.append("<br/>");
         }
-        webView.loadData(String.format(Locale.US,htmlHead,fontSize,printFontSize)+ss.toString()+htmlFooter,"text/html; charset=utf-8","utf-8");
+        webView.loadData(String.format(Locale.US, htmlHead, fontSize, printFontSize) + ss.toString() + htmlFooter, "text/html; charset=utf-8", "utf-8");
         // createWebPrintJob(webView);
     }
 
-    private void fileBePrinted(Uri uri){
+    private void fileBePrinted(Uri uri) {
         BufferedReader br = null;
         StringBuilder sb = new StringBuilder();
         String line;
@@ -183,31 +189,33 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
             }
         }
 
-        webView.loadData(String.format(Locale.US,htmlHead,fontSize,printFontSize)+sb.toString()+htmlFooter,"text/html; charset=utf-8","utf-8");
+        webView.loadData(String.format(Locale.US, htmlHead, fontSize, printFontSize) + sb.toString() + htmlFooter, "text/html; charset=utf-8", "utf-8");
         // createWebPrintJob(webView);
     }
 
 
     public boolean onCreateOptionsMenu(Menu menu) {
+        MenuCompat.setGroupDividerEnabled(menu, true);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-         menu.add(0,1,1,"Grant READ");
+            menu.add(0, 1, 1, "Grant READ");
         }
-        menu.add(0,2,2,"Font A");
-        menu.add(0,3,3,"Font B");
-        menu.add(0,4,4,"Font C");
-        menu.add(0,5,5,"Font D");
-
-        menu.add(0,6,6,"Buy me a coffee");
+        menu.add(1, 7, 2, "Demo text");
+        menu.add(1, 9, 4, "Insert clipboard");
+        menu.add(2, 2, 6, "Font A");
+        menu.add(2, 3, 7, "Font B");
+        menu.add(2, 4, 8, "Font C");
+        menu.add(2, 5, 9, "Font D");
+        menu.add(4, 6, 13, "Buy me a coffee");
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id=item.getItemId();
-        if(id==android.R.id.home){
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
             finish();
-        }else if(id==1){
+        } else if (id == 1) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
@@ -217,38 +225,52 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
 
                 }
             }
-        }else if(id==2){
+        } else if (id == 2) {
             saveFont(1);
             setFont(1);
-        }else if(id==3){
+        } else if (id == 3) {
             saveFont(2);
             setFont(2);
-        }else if(id==4){
+        } else if (id == 4) {
             saveFont(3);
             setFont(3);
-        }else if(id==5){
+        } else if (id == 5) {
             saveFont(4);
             setFont(4);
-        }else if(id==6){
-            if(bp!=null) {
+        } else if (id == 6) {
+            if (bp != null) {
                 bp.purchase(this, "coffee");
-            }else{
-                Toast.makeText(this, R.string.no_bp,Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, R.string.no_bp, Toast.LENGTH_SHORT).show();
             }
+        } else if (id == 7) {
+            htmlBody = readAssets("html.html");
+            webView.loadData(String.format(Locale.US, htmlHead, fontSize, printFontSize) + htmlBody + htmlFooter, "text/html; charset=utf-8", "utf-8");
+
+        } else if (id == 9) {
+            try {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                ClipData buf = Objects.requireNonNull(clipboard.getPrimaryClip());
+                stringBePrinted(buf.getItemAt(0).coerceToText(this).toString());
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+
+
         }
         return super.onOptionsItemSelected(item);
     }
 
     @SuppressLint("ApplySharedPref")
-    private void saveFont(int i){
+    private void saveFont(int i) {
         sPref = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor ed = sPref.edit();
         ed.putInt(SAVED_SIZE, i);
         ed.commit();
     }
 
-    private void setFont(int s){
-        switch (s){
+    private void setFont(int s) {
+        switch (s) {
             case 1:
                 fontSize = 17;
                 printFontSize = 26;
@@ -284,7 +306,7 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
 
     @Override
     public void onProductPurchased(@NonNull String productId, @Nullable TransactionDetails details) {
-        webView.loadData(String.format(Locale.US,htmlHead,fontSize,printFontSize)+"<h1>Thanks !</h1>"+htmlFooter,"text/html; charset=utf-8","utf-8");
+        webView.loadData(String.format(Locale.US, htmlHead, fontSize, printFontSize) + "<h1>Thanks !</h1>" + htmlFooter, "text/html; charset=utf-8", "utf-8");
         bp.consumePurchase(productId);
     }
 
@@ -304,29 +326,30 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
     }
 
 
-    public static class PrintDocumentAdapterWrapper extends PrintDocumentAdapter{
+    public static class PrintDocumentAdapterWrapper extends PrintDocumentAdapter {
 
         private final PrintDocumentAdapter delegate;
-        PrintDocumentAdapterWrapper(PrintDocumentAdapter adapter){
+
+        PrintDocumentAdapterWrapper(PrintDocumentAdapter adapter) {
             super();
             this.delegate = adapter;
         }
 
         @Override
         public void onLayout(PrintAttributes oldAttributes, PrintAttributes newAttributes, CancellationSignal cancellationSignal, LayoutResultCallback callback, Bundle extras) {
-            delegate.onLayout(oldAttributes, newAttributes,  cancellationSignal, callback,  extras);
-            Log.d("ANTSON","onLayout");
+            delegate.onLayout(oldAttributes, newAttributes, cancellationSignal, callback, extras);
+            Log.d("ANTSON", "onLayout");
         }
 
         @Override
         public void onWrite(PageRange[] pages, ParcelFileDescriptor destination, CancellationSignal cancellationSignal, WriteResultCallback callback) {
-            delegate.onWrite( pages, destination,cancellationSignal,callback);
-            Log.d("ANTSON","onWrite");
+            delegate.onWrite(pages, destination, cancellationSignal, callback);
+            Log.d("ANTSON", "onWrite");
         }
 
-        public void onFinish(){
+        public void onFinish() {
             delegate.onFinish();
-            Log.d("ANTSON","onFinish");
+            Log.d("ANTSON", "onFinish");
         }
 
     }
@@ -345,9 +368,9 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
 
         //open print dialog
         if (printManager != null) {
-            printManager.print(jobName, printAdapter, new PrintAttributes.Builder().setMinMargins(new PrintAttributes.Margins(0,0,0,0)).build());
-        }else{
-            webView.loadData(String.format(Locale.US,htmlHead,fontSize,printFontSize)+"PrintManager is null"+htmlFooter,"text/html; charset=utf-8","utf-8");
+            printManager.print(jobName, printAdapter, new PrintAttributes.Builder().setMinMargins(new PrintAttributes.Margins(0, 0, 0, 0)).build());
+        } else {
+            webView.loadData(String.format(Locale.US, htmlHead, fontSize, printFontSize) + "PrintManager is null" + htmlFooter, "text/html; charset=utf-8", "utf-8");
 
         }
     }
